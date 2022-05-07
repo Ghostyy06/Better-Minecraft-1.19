bc76e8cbd74c6c82f6900181f63ce548d25c5004
Ghostyy06 <jkoproski06@gmail.com>
2022-05-07 19:31:12 -0400

    slightly better mob spawners

--- src/main/java/net/ghostyy/betterminecraft/mixin/MobSpawnerLogicMixin.java
+++ src/main/java/net/ghostyy/betterminecraft/mixin/MobSpawnerLogicMixin.java
@@ -6,6 +6,8 @@ import org.spongepowered.asm.mixin.Mixin;
 @Mixin(MobSpawnerLogic.class)
 public abstract class MobSpawnerLogicMixin {
 
-    private int minSpawnDelay = 0;
-    private int maxSpawnDelay = 1;
+    private int minSpawnDelay = 200;
+    private int maxSpawnDelay = 600;
+    private int spawnCount = 5;
+    private int maxNearbyEntities = 8;
 }
