e163b3baba755ca17654b1024fc51135d47dea21
Ghostyy06 <jkoproski06@gmail.com>
2022-06-09 17:40:50 -0400

    loot table hotfix

--- src/main/java/net/ghostyy/betterminecraft/Better_Minecraft_Client.java
+++ src/main/java/net/ghostyy/betterminecraft/Better_Minecraft_Client.java
@@ -1,8 +1,12 @@
 package net.ghostyy.betterminecraft;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.fabricmc.fabric.api.client.screenhandler.v1.ScreenRegistry;
 import net.ghostyy.betterminecraft.item.custom.TriBowItem;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.screen.ScreenHandlerType;
 
 public class Better_Minecraft_Client implements ClientModInitializer {
 
